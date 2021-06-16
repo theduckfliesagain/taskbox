0a1c8fc6baecece94740e2980c0a6816dd433c3f
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-16 13:45:41 +0100

    fix text overflow and update story

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,7 @@ export default function Task({ task: { id, state, title } , onArchiveTask, onPin
                 <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
             </label>
             <div className="title">
-                <input type="text" value={title} readOnly={true} placeholder="Input title" />
+                <input type="text" value={title} readOnly={true} placeholder="Input title" style={{ textOverflow: 'ellipsis' }}/>
             </div>
 
             <div className="actions" onClick={event => event.stopPropagation()}>
